c148502ebdfa9635baea86af03028bfb6471b984
Hamza Keurti <hamza.keurti@gmail.com>
2019-01-11 15:50:43 +0800

    Added generic data loader.

---
+++ load/loader.py
@@ -0,0 +1,19 @@
+import json
+import os
+import pandas as pd
+
+from pickles import pickling
+
+
+def load_df_pubs(json_path,pickles_path=None,refresh_pickle=False):
+    if (not os.path.exists(pickles_path)) or refresh_pickle:
+        with open(json_path, 'rb') as infile:
+            raw_data = json.load(infile)
+            names = raw_data.keys()
+            pubs_dfs = {auth_name:pd.DataFrame(raw_data[auth_name]) for auth_name in names}
+            pickling.pickle_object(pubs_dfs, pickles_path)
+    else:
+        pubs_dfs = pickling.unpickle_object(pickles_path)
+    return pubs_dfs
+
+
