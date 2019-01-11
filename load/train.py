37c05159454b778563b2ea1d22ddded56b38a68b
Hamza Keurti <hamza.keurti@gmail.com>
2019-01-11 15:51:38 +0800

    Loading training pubs data

--- load/train.py
+++ load/train.py
@@ -3,24 +3,18 @@ import os
 import pandas as pd
 
 from pickles import pickling
+from load import loader
 
 DATA_DIRECTORY = 'data/'
 PUBS_TRAIN_PATH = DATA_DIRECTORY + 'pubs_train.json'
 ASSIGNMENT_TRAIN_PATH = DATA_DIRECTORY + 'assignment_train.json'
 
 
-def load_pubs_train(refresh_pickle=False):
-    if (not os.path.exists(pickling.PUBS_TRAIN_DF)) or refresh_pickle:
-        with open(PUBS_TRAIN_PATH, 'rb') as infile:
-            raw_data = json.load(infile)
-            df = pd.DataFrame.from_dict(raw_data)
-            pickling.pickle_object(df, pickling.PUBS_TRAIN_DF)
-    else:
-        df = pickling.unpickle_object(pickling.PUBS_TRAIN_DF)
-    return df
+def load_df_pubs(refresh_pickle=False):
+    return loader.load_df_pubs(json_path=PUBS_TRAIN_PATH,pickles_path=pickling.PUBS_TRAIN_DF,refresh_pickle=refresh_pickle)
 
 
-def load_assignment_train(refresh_pickle=False):
+def load_assignment(refresh_pickle=False):
     if (not os.path.exists(pickling.ASSIGNMENT_TRAIN_DF)) or refresh_pickle:
         with open(ASSIGNMENT_TRAIN_PATH, 'rb') as infile:
             raw_data = json.load(infile)
